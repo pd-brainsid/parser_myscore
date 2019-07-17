8d4dc6a261707fad6f69e82ac786618b68ac5eda
MykhailoKlimchuk <mikeklimchuck@gmail.com>
2019-07-17 13:48:36 +0300

    add auth

--- app.py
+++ app.py
@@ -1,36 +1,59 @@
-from flask import Flask, jsonify
-import db
-import logging
+from flask import Flask
+from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
 
-APP = Flask(__name__)
-logging.basicConfig(filename="log.log", level=logging.INFO)
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'super-secret'
+
+api = Api(app, prefix="/api/v1")
+
+USER_DATA = {
+    "user": "1111"
+}
 
 
-@APP.route('/')
-def index():
-    return {
-        'INFO': 'Hello! You can get info about completed soccer or hockey games for today'
-    }
+class User(object):
+    def __init__(self, id):
+        self.id = id
+
+    def __str__(self):
+        return "User(id='%s')" % self.id
 
 
-@APP.route('/api/games/<string:sport_name>', methods=['GET'])
-def get_games(sport_name):
+def verify(username, password):
+    if not (username and password):
+        return False
+    if USER_DATA.get(username) == password:
+        return User(id=123)
 
-    logging.info(f"request for information about {sport_name}")
-    games_info = db.get_games(sport_name)
-    if len(games_info) == 0:
-        answer = {
-            'INFO': 'There is no information about this game for today'
-        }
-    else:
-        answer = {
-            'INFO': 'There is information about this game for today',
-            sport_name: games_info
-        }
-    return answer
 
+def identity(payload):
+    user_id = payload['identity']
+    return {"user_id": user_id}
+
+
+jwt = JWT(app, verify, identity)
+
+
+class GamesResources(Resource):
+    @jwt_required()
+    def get(self, sport_name):
+        import db
+
+        games_info = db.get_games(sport_name)
+        if len(games_info) == 0:
+            answer = {
+                'INFO': 'There is no information about this game for today'
+            }
+        else:
+            answer = {
+                'INFO': 'There is information about this game for today',
+                sport_name: games_info
+            }
+        return answer
+
+
+api.add_resource(GamesResources, '/games/<string:sport_name>')
 
 if __name__ == '__main__':
-    logging.info(f"--------app started--------")
-
-    APP.run(debug=True)
+    app.run(debug=True)
